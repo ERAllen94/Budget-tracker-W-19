6e36d0f6594c9586b82b6093975815458f226c41
Evan Allen <jacob_allen1994@yahoo.com>
2020-12-15 16:35:19 -0700

    added readme

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Title:
 Budget tracker app
 
-# heroku link
+
+# Description
+You can keep track of your budget online or offline!
+
+# Screenshots
+
+![alt text](./Images/budget-tracker-1.PNG)
+![alt text](./Images/budget-tracker-2.PNG)
+
+
+
+
+# links
+https://github.com/ERAllen94/Budget-tracker-W-19
 
--- public/service-worker.js
+++ public/service-worker.js
@@ -25,7 +25,7 @@ const FILES_TO_CACHE =[
 self.addEventListener('install', function (e){
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache){
-            console.log('installing cache :' + CACHENAME)
+            console.log('installing cache :' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
     )
